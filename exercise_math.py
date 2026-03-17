fdc120e3566d087b001ec70742207c73976ad69e
damaralp <damaralpaz@mail.austral.edu.ar>
2026-03-17 10:54:13 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,20 @@ def math():
     """
     a = 57
     b = 7
+
+    suma = a + b
+    print(suma)
+    resta = a - b
+    print(resta)
+    producto = a * b
+    print(producto)
+    promedio= suma/2
+    print(promedio)
+    cociente= a//b
+    print(cociente)
+    resto = a % b
+    print(resto)
+    division = a/b
+    print(division)
+
+math()
